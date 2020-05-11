418e04f064428828f2d6d4541c7d378ffe4bd5ba
shubhvish4495 <shubhamsaurav001@gmail.com>
2020-05-12 01:31:24 +0530

    Updated Code base

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 
  A pipeline code to fetch data from twitter API to Mongo DB database.
 
- install dependencies using install_dependencies.bat file 
+ Install dependencies using install_dependencies.bat/installDependencies.sh file 
 
- set up database and twitter API related configuration in configuration.py file
+ Set up an account for twitter developers https://developer.twitter.com/en
 
- run the code using the command python tweet.py
+ After account set up, create an app to get access credentials for using the twitter API. 
+
+ Set up database and twitter API related configuration for project in configuration.py file
+
+ Run the code using the command python tweet.py
--- mongoDbOperations.py
+++ mongoDbOperations.py
@@ -4,10 +4,19 @@ from pprint import pprint
 
 database = mongo.connect()
 
+class tweetObject:
+    def __init__(self, username, created_at, tweet, retweet_count, place, location):
+        self.username = username
+        self.created_at = created_at
+        self.tweet = tweet
+        self.retweet_count = retweet_count
+        self.place = place
+        self.location = location
+    
 def insertTweets(username, created_at, tweet, retweet_count, place, location):
     try:
         collection = database[cfg.MONGO_COLLECTION_NAME]
-        insertDict = {"username" : username, "created_at" : created_at, "tweet" : tweet, "retweet_count": retweet_count, "place": place, "location" : location}
-        insertStatus = collection.insert_one(insertDict)
+        singleTweet = tweetObject(username, created_at, tweet, retweet_count, place, location)
+        collection.insert_one(singleTweet.__dict__)
     except Exception as e:
         print("Error in inserting tweet into mongo :\n",e)
